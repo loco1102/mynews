d3f503bcf26d740760850d20cceb9d08eecd52d6
ryuhei <ryuhei1102@gmail.com>
2020-03-22 15:04:54 +0000

    web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/creat', 'Admin\NewsController@add');
+});
+
+//kadai3
+Route::group(['prefix' => 'user'], function() {
+    Route::get('profile/creat', 'User\ProfileController@add');
+});
+
+//kadai4
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('admin/profile/creat', 'Admin\ProfileController@add');
+    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+});
